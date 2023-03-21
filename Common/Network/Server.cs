ed783350032b9a25eba0e39be7bb0acb8320e115
이건호 <k.h.leedev@gmail.com>
2023-03-21 22:50:23 +0900

    GS-4 소켓 서버 구조 변경

--- Common/Network/Server.cs
+++ Common/Network/Server.cs
@@ -5,54 +5,88 @@ using System.Text;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
-using Common.Object;
+using Common.Packet;
 
 namespace Common.Network
 {
     public class Server
     {
-        public Server()
-        {
+        private TcpListener _listener;
+        private int _port;
+        private bool _running;
 
+        public Server(int port)
+        {
+            _port = port;
+            _listener = new TcpListener(IPAddress.Any, port);
+            _running = false;
         }
 
-        public async Task Start()
+        public async Task StartAsync()
         {
-            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(new IPEndPoint(IPAddress.Any, 11000));
-            listener.Listen(100);
-
-            while (true)
+            if (!_running)
             {
-                Socket client = await listener.AcceptAsync();
-                Console.WriteLine($"Client connected: {client.RemoteEndPoint}");
-
-                Task.Factory.StartNew(async () =>
+                _listener.Start();
+                _running = true;
+                Console.WriteLine("Server started on port " + _port);
+                while (_running)
                 {
-                    try
-                    {
-                        byte[] buffer = new byte[1024];
-                        var bytesReceived = client.Receive(buffer);
-
-                        Packet packet;
-                        using (MemoryStream stream = new MemoryStream(buffer, 0, bytesReceived))
-                        {
-                            packet = JsonSerializer.Deserialize<Packet>(stream);
-                        }
-
-                        /*
-                        byte[] message = Encoding.ASCII.GetBytes("Hello Client~");
-                        int bytesSent = client.Send(message);
-
-                        Console.WriteLine($"Data sent: {Encoding.ASCII.GetString(message)}");
-                        */
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Exception: {ex.Message}");
-                    }
-                });
+                    TcpClient client = await _listener.AcceptTcpClientAsync();
+                    Task.Run(() => HandleClientAsync(client));
+                }
             }
+        }
+
+        public void Stop()
+        {
+            if (_running)
+            {
+                _running = false;
+                _listener.Stop();
+                Console.WriteLine("Server stopped");
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            try
+            {
+                Console.WriteLine("Client connected: " + client.Client.RemoteEndPoint);
+
+                NetworkStream stream = client.GetStream();
+
+                while (_running && client.Connected)
+                {
+                    // Receive the packet from the client
+                    Packet receivedPacket = await Packet.ReceiveAsync(stream);
+
+                    // Handle the received packet in parallel
+                    Packet responsePacket = await Task.Run(() => HandlePacketAsync(receivedPacket));
+
+                    // Send the response packet to the client
+                    await responsePacket.SendAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine("Client disconnected: " + client.Client.RemoteEndPoint);
+            }
+        }
+
+        private async Task<Packet> HandlePacketAsync(Packet packet)
+        {
+            // Process the received packet here in a parallel-friendly way
+            // ...
+
+            // Create and return the response packet
+            Packet responsePacket = new Packet();
+            // ...
+            return responsePacket;
         }
     }
 }
